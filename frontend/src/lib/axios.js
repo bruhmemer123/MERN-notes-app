2a23f1058faf0253a028ada4c16d0618cfec8af8
bruhmemer123 <afydking@gmail.com>
2026-04-03 01:15:21 +0530

    Add comma to axios instance configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 const baseURL = process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5001/api";
 const api=axios.create({
-    baseURL: baseURL
+    baseURL: baseURL,
     withCredentials: true
 })
 export default api
