efd25ae2d73c633367d9cb2d24903662bfe5f2b1
Affan <afydking@gmail.com>
2026-08-15 17:07:41 +0530

    ultra finals

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,14 @@ import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
 import path from "path"
+import { fileURLToPath } from "url";
 
 dotenv.config()
 
 const app=express();
 const PORT=process.env.PORT
-const __dirname=path.resolve()
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 if(process.env.NODE_ENV!=="production"){
 app.use(cors({
     origin:"http://localhost:5173"
@@ -21,9 +23,9 @@ app.use(rateLimiter)
 app.use("/api/notes",notesRoutes)
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.use(express.static(path.resolve(__dirname,"../frontend/dist")))
     app.get("/*splat",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    res.sendFile(path.join(path.resolve(__dirname,"../frontend/dist"),"index.html"))
 })}
 
 connectDB().then(()=>{
